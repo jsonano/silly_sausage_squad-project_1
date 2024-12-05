5e8c99470af593b51f2408e17ee9f56484fd685d
jsonano <jchao5768@gmail.com>
2024-12-04 21:23:16 -0500

    Implemented creating DBs and adding users

--- app/database.py
+++ app/database.py
@@ -1,13 +1,29 @@
 import sqlite3
 
-db = sqlite.connect("apis")
+db = sqlite3.connect("apis")
 c = db.cursor()
 
 def create_db():
     c.execute('''
         CREATE TABLE IF NOT EXISTS logins (
-        user_id AUTO_INCREMENT,
-        username TEXT,
-        password_hash TEXT);'''
-    )
-    c.execute("CREATE TABLE apis (request_id AUTO_INCREMENT,  )")
+        user_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+        username TEXT NOT NULL,
+        password_hash TEXT NOT NULL,
+    );
+    ''')
+    
+    c.execute('''
+        CREATE TABLE apis (
+        request_id INTEGER PRIMARY KEY AUTO_INCREMENT,  
+        foreign key(user_id) REFERENCES logins(user_id),
+        api_name TEXT NOT NULL,
+        request TEXT NOT NULL,
+        response TEXT NOT NULL,
+    );
+    ''')
+
+def add_user(username, password_hash):
+    c.execute("INSERT INTO logins (username, password_hash) VALUES (?, ?)", (username, password_hash))
+    db.commit()
+
+db.close()
