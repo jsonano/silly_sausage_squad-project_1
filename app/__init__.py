12830bee737d0c850a075602f38c3c81ccb528eb
Alex Luo <aluo50@stuy.edu>
2024-12-05 23:41:30 -0500

    added login/logout templates and routing, also fixed small db error

--- app/__init__.py
+++ app/__init__.py
@@ -9,26 +9,65 @@
 from flask import Flask, request, render_template, redirect, url_for, flash, session
 import os
 import sqlite3
+from database import create_db, add_user, return_user   
+
+
+db_filename = "apis.db"
 
 # Auto-generated secret key
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY') or os.urandom(32)
 
+create_db()
 
 
 @app.route('/')
 def home():
-    #if "username" in session:
-    #    return render_template("home.html", username=session["username"])
-    #else:
-    #    return redirect(url_for("login"))
-    return render_template("home.html", username="test")
+    if "username" in session:
+        return render_template("home.html", username=session["username"])
+    return redirect(url_for("login"))
 
-'''
-@app.route('login', methods=["GET", "POST"])
+
+@app.route('/login', methods=["GET", "POST"])
 def login():
-   return render_template("login.html")
-'''
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+    
+        user = return_user(username)
+
+        if user and user[2] == password:  # Match plaintext password
+            session["username"] = username
+            return redirect(url_for("home"))
+        else:
+            return render_template("login.html", error_message="Invalid username or password")
+
+    return render_template("login.html")
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        existing_user = return_user(username)
+        
+        if existing_user:
+            error_message = "User already in database"
+            return render_template("register.html", error_message=error_message)
+        
+        add_user(username, password)
+
+        session["username"] = username
+        return redirect(url_for("home"))
+
+    return render_template("register.html")
+
+@app.route('/logout')
+def logout():
+    if "username" in session:
+        session.pop("username")
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run(debug=True)
